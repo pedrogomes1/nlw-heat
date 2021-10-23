a172d71e4a5f9c7cbda8ccabdadccc604c8cdedb
Pedro <pedro-gomes17@hotmail.com>
2021-10-23 14:27:05 -0300

    new exception handling block in promise

--- backend/src/controllers/AuthenticateUserController.ts
+++ backend/src/controllers/AuthenticateUserController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
-
 class AuthenticateUserController {
   async handle(request: Request, response: Response) {
     const { code } = request.body;
     
     const service = new AuthenticateUserService();
-    const result = await service.execute(code); 
-    return response.json(result);
+    try {
+      const result = await service.execute(code); 
+      return response.json(result);
+    } catch (error: any) {
+      return response.json({error: error.message})
+    }
   }
 }
 
