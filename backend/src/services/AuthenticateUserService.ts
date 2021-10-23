df6f8a82c37ef637b54e650904c3203584020373
Pedro <pedro-gomes17@hotmail.com>
2021-10-23 14:29:23 -0300

    new logic for github user return with token and creation if it doesn't exist in db

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import prismaClient from '../prisma'
+import { Secret, sign } from 'jsonwebtoken';
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -33,6 +35,39 @@ class AuthenticateUserService {
         authorization: `Bearer ${access_token}`
       }
     })
+
+    const { login, id, avatar_url, name } = response.data;
+    
+    const user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+
+    if(!user) {
+      const user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        }
+      })
+      const token = sign({ user: {
+        name: user.name,
+        avatar_url: user.avatar_url,
+        id: user.id
+      }},
+      process.env.JWT_SECRET as Secret,
+        {
+          subject: user.id,
+          expiresIn: '1d'
+        }
+      );
+
+      return { token, user }
+    }
+
 
     return response.data;
   }
