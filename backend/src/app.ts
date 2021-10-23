dea114c46fa627256660d693a16d592e0f1e6fc4
Pedro <pedro-gomes17@hotmail.com>
2021-10-23 15:50:42 -0300

    settings for using the io socket

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,25 @@
 import "dotenv/config"
 import express from "express";
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
+
 import { router } from './routes';
 
 const app = express();
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado ${socket}`)
+})
+
 app.use(express.json())
 app.use(router)
 
@@ -16,4 +33,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code);
 })
 
-app.listen(4000, () => console.log(`Server is running on PORT 4000`))
+export { serverHttp, io }
