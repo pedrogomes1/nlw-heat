72cce01a5e6de99ee582e9203c72d88ba2cd7f4e
Pedro <pedro-gomes17@hotmail.com>
2021-10-24 12:54:01 -0300

    new condition for displaying the new message registration form or for logging into github

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,18 @@ import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
 
 import styles from './App.module.scss';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/auth';
+import { SendMessageForm } from './components/SendMessageForm';
 
 export function App() {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   )
 }
