680aad8fec4ad72bb3cada3a2e9baecadd62f5ad
Pedro <pedro-gomes17@hotmail.com>
2021-10-28 23:11:23 -0300

    chore: new alignItems style to header container

--- mobile/src/components/Header/styles.ts
+++ mobile/src/components/Header/styles.ts
@@ -6,6 +6,7 @@ export const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     paddingHorizontal: 20,
   },
   logoutButton: {
