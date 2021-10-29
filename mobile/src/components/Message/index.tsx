f03a2f401d3d534b70f017ba17c88af9f6b4a59b
Pedro <pedro-gomes17@hotmail.com>
2021-10-28 23:19:44 -0300

    chore: added dynamic message display data types

--- mobile/src/components/Message/index.tsx
+++ mobile/src/components/Message/index.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
-
 import {
   Text,
   View
 } from 'react-native';
+
 import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
 
-export function Message(){
+export type MessageProps = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+type Props = {
+  data: MessageProps;
+}
+
+export function Message({ data }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.message}>
-        Texto da mensagem
+        {data.text}
       </Text>
 
       <View style={styles.footer}>
         <UserPhoto
-          imageUri="https://github.com/PedroGomes1.png"
+          imageUri={data.user.avatar_url}
           sizes="SMALL"
          />
 
         <Text style={styles.username}>
-          Pedro Gomes
+          {data.user.name}
         </Text>
       </View>
     </View>
