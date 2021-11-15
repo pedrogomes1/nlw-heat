925d41fa3472e0cfb592036e431b15d8b5c0c2b2
Pedro <pedro-gomes17@hotmail.com>
2021-11-15 17:51:42 -0300

    feat: added functionality to list messages in real time

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+
+const apiUrl = `${api.defaults.baseURL}`;
+const socket = io(apiUrl);
+
+const messagesQueue: MessageProps[] = [];
+
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage)
+})
 
 export function MessageList() {
 
@@ -16,6 +26,17 @@ export function MessageList() {
       setCurrentMessages(response.data);
     }
     fetchMessages();
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
   }, [])
 
   return (
