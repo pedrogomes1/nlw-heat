2b051d7393c410e638079753b4ba75b612a9e8e7
Pedro <pedro-gomes17@hotmail.com>
2021-11-15 16:58:59 -0300

    feat: added fetch api route to get last messages

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
+import { api } from '../../services/api';
 
-import { Message } from '../Message';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
+
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const response = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(response.data);
+    }
+    fetchMessages();
+  }, [])
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message />
-      <Message />
-      <Message />
+      {currentMessages.map(message => (
+         <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   );
 }
