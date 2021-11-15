0bc125e759c54393fc0d4e9ecd646a8046b74ffc
Pedro <pedro-gomes17@hotmail.com>
2021-11-15 17:28:07 -0300

    chore: added new function to register new messages connecting with backend

--- mobile/src/components/SendMessageForm/index.tsx
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
 import {
+  Alert,
+  Keyboard,
   TextInput,
   View
 } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import { Button } from '../Button';
 
@@ -13,6 +16,27 @@ export function SendMessageForm() {
 
   const [message, setMessage] = useState('');
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim();
+
+    if (!messageFormatted) {
+      return Alert.alert('Escreva a mensagem para enviar!')
+    }
+
+    setSendingMessage(true);
+
+    try {
+      await api.post('/messages', { message: messageFormatted });
+      setMessage('');
+      Keyboard.dismiss();
+      Alert.alert('Mensagem enviada com sucesso!')
+    } catch (error) {
+      Alert.alert(`Erro ao enviar a mensagem!, ${error}`)
+    } finally {
+      setSendingMessage(false);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -32,6 +56,8 @@ export function SendMessageForm() {
         title="ENVIAR MENSAGEM"
         backgroundColor={COLORS.PINK}
         color={COLORS.WHITE}
+        onPress={handleMessageSubmit}
+        isLoading={sendingMessage}
       />
     </View>
   );
